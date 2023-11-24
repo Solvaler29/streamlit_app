9425ccbc8d85bbd1376b6d74e7cfea57576d796d
JimenaVegaS <89529401+JimenaVegaS@users.noreply.github.com>
2023-11-23 23:34:50 -0500

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+titulos_pestanas = ['Página principal', 'Nacional', 'Internacional','Departamentos','Países','Sobre nosotras']
+pestaña1, pestaña2, pestaña3, pestaña4, pestaña5, pestaña6 = st.tabs(titulos_pestanas)
+
+with pestaña1:
+    st.title('Análisis de Población identificada con DNI de mayor de edad por condición de donante de órganos')
+    st.write("Texto sobre donación de órganos")
+    st.write("")
+    with st.container():
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.button("Nacional", type="secondary")
+            chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+            st.bar_chart(chart_data)
+        with right_column:
+            st.button("Internacional", type="secondary") 
+            chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+            st.bar_chart(chart_data)
+            st.caption('Los datos de este gráfico no están actualizados a la fecha actual.')
+
+
+
+
+st.link_button("Para más información de click aquí", "https://www.datosabiertos.gob.pe/dataset/reniec-poblaci%C3%B3n-identificada-con-dni-de-mayor-de-edad-por-condici%C3%B3n-de-donante-de-%C3%B3rganos")
 

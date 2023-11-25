002621acb11b717cc354b34e72e66641fdc5d0ca
JimenaVegaS <89529401+JimenaVegaS@users.noreply.github.com>
2023-11-25 17:45:22 -0500

    grafico

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,14 +57,8 @@ with pestaña4:
         ("2022","2023"))
     mylist = ["Áncash","Amazonas","Apurímac","Arequipa","Ayacucho","Cajamarca","Callao","Cusco","Huánuco","Huancavelica","Ica","Junín","La Libertad","Lambayeque","Lima","Loreto","Madre de Dios","Moquegua","Pasco","Piura","Puno","San Martín","Tacna","Tumbes",]
     if option0 == "2022":
-        chart_data = pd.DataFrame(
-            {
-                "Departamento": list(range(20)) * 3,
-                "Cantidad": np.random.randn(60),
-                "Condición de donante": ["Donante"] * 20 + ["No donante"] * 20 + ["No especifica"] * 20,
-            }
-        )
-        st.bar_chart(chart_data, x="Departamento", y="Cantidad", color="Condición de donante")
+        chart_data = pd.DataFrame(np.random.randn(20, 1), columns=["a"])
+        st.bar_chart(chart_data)
     elif option0 == "2023":
         chart_data = pd.DataFrame(
             {

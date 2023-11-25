2b82c108532c289e61457162f5191c69037134db
JimenaVegaS <89529401+JimenaVegaS@users.noreply.github.com>
2023-11-25 17:26:01 -0500

    grafico

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,21 +58,21 @@ with pestaña4:
     if option0 == "2022":
         chart_data = pd.DataFrame(
             {
-                "col1": list(range(20)) * 3,
-                "col2": np.random.randn(60),
-                "col3": ["A"] * 20 + ["B"] * 20 + ["C"] * 20,
+                "Donantes": list(range(20)) * 3,
+                "No donantes": np.random.randn(60),
+                "No especifica": ["A"] * 20 + ["B"] * 20 + ["C"] * 20,
             }
         )
-        st.bar_chart(chart_data, x="col1", y="col2", color="col3")
+        st.bar_chart(chart_data, x="Donantes", y="No donantes", color="No especifica")
     elif option0 == "2023":
         chart_data = pd.DataFrame(
             {
-                "col1": list(range(20)) * 3,
-                "col2": np.random.randn(60),
-                "col3": ["A"] * 20 + ["B"] * 20 + ["C"] * 20,
+                "Donantes": list(range(20)) * 3,
+                "No donantes": np.random.randn(60),
+                "No especifica": ["A"] * 20 + ["B"] * 20 + ["C"] * 20,
             }
         )
-        st.bar_chart(chart_data, x="col1", y="col2", color="col3")
+        st.bar_chart(chart_data, x="Donantes", y="No donantes", color="No especifica")
 
 with pestaña5:
     st.title("Condición de donante de órganos por países")

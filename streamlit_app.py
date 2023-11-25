6aec3069b445e4a14fb280dfbdbe06dc1cceddf2
JimenaVegaS <89529401+JimenaVegaS@users.noreply.github.com>
2023-11-25 17:38:11 -0500

    grafico

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ with pestaña4:
     if option0 == "2022":
         chart_data = pd.DataFrame(
             {
-                "Departamento": list[mylist] * 3,
+                "Departamento": list["Áncash","Amazonas","Apurímac","Arequipa","Ayacucho","Cajamarca","Callao","Cusco","Huánuco","Huancavelica","Ica","Junín","La Libertad","Lambayeque","Lima","Loreto","Madre de Dios","Moquegua","Pasco","Piura","Puno","San Martín","Tacna","Tumbes","Ucayali"],
                 "Cantidad": np.random.randn(60),
                 "Condición de donante": ["Donante"] * 20 + ["No donante"] * 20 + ["No especifica"] * 20,
             }

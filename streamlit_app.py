03a49fdc86becb36a882433833853ca955def79b
JimenaVegaS <89529401+JimenaVegaS@users.noreply.github.com>
2023-11-25 17:51:54 -0500

    grafica

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,12 +55,11 @@ with pestaña4:
     option0 = st.selectbox(
         "Elige un año",
         ("2022","2023"))
-    mylist = ["Áncash","Amazonas","Apurímac","Arequipa","Ayacucho","Cajamarca","Callao","Cusco","Huánuco","Huancavelica","Ica","Junín","La Libertad","Lambayeque","Lima","Loreto","Madre de Dios","Moquegua","Pasco","Piura","Puno","San Martín","Tacna","Tumbes",]
     if option0 == "2022":
-        chart_data = pd.DataFrame(np.random.randn(20, 1), columns=["a"])
+        chart_data = pd.DataFrame(np.random.randn(25, 1), columns=["a"])
         st.bar_chart(chart_data)
     elif option0 == "2023":
-        chart_data = pd.DataFrame(np.random.randn(20, 1), columns=["a"])
+        chart_data = pd.DataFrame(np.random.randn(25, 1), columns=["a"])
         st.bar_chart(chart_data)
 
 with pestaña5:

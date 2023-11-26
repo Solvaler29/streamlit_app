d7f68398caf73d1e85fd2ef56d741e26a6cae75e
JimenaVegaS <89529401+JimenaVegaS@users.noreply.github.com>
2023-11-26 12:11:59 -0500

    graf

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,12 +60,10 @@ with pestaña4:
         df = pd.read_csv("marzo2022de18a80años(1).csv")
         df[['Departamento',"C_Donacion","Cantidad"]]
         "Donantes por Departamento"
-        source = pd.DataFrame(
-            {
+        source = pd.DataFrame({
             "Cantidad" : [11657,109422,16666,185178,31964,62131,62131,95125,13218,29837,95206,58440,186959,110010,1575708,41149,20438,18486,14127,134095,38182,51936,27444,16078,40806]
             "Departamentos" : ["Amazonas","Áncash","Apurímac","Arequipa","Ayacucho","Callao","Cajamarca","Cuzco","Huancavelica","Huánuco","Ica","Junín","La Libertad","Lambayeque","Lima","Loreto","Madre de Dios","Moquegua","Pasco","Piura","Puno","San Martín","Tacna","Tumbes","Ucayali"]
-            }
-        )
+        })
         bar_chart = alt.Chart(source).mark_bar().encode(
             y = "Cantidad",
             x = "Departamentos",

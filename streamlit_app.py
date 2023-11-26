7f9510f8742d4dff1bb87b3941ad131a49a1e9dc
JimenaVegaS <89529401+JimenaVegaS@users.noreply.github.com>
2023-11-26 11:07:41 -0500

    graf

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-import pandas as pd
+import altair as alt
+from urllib.error import URLError
 
 titulos_pestanas = ['Página principal', 'Nacional', 'Internacional','Departamentos','Países','Sobre nosotras']
 pestaña1, pestaña2, pestaña3, pestaña4, pestaña5, pestaña6 = st.tabs(titulos_pestanas)

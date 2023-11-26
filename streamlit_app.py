d6051eac078f5c0b55219ef16dc2c02d099ba366
JimenaVegaS <89529401+JimenaVegaS@users.noreply.github.com>
2023-11-26 10:33:28 -0500

    graf

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,8 @@ with pestaña4:
         ("2022","2023"))
     if option0 == "2022":
         df = pd.read_csv("Junio2022de18a80años.csv")
-        df['Departamento','C_Donacion']
+        df
+        df['Departamento']
         chart_data = pd.DataFrame(np.random.randn(25, 1), columns=["cantidad"])
         st.bar_chart(chart_data)
 

de57c3062f92f185b3f801dba74fd1c83b8e1572
JimenaVegaS <89529401+JimenaVegaS@users.noreply.github.com>
2023-11-26 14:47:51 -0500

    rgaf

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ with pestaña4:
         df = pd.read_csv("df_si_aceptan_peru_mar23.csv")
         df[['Departamento',"Donacion","Cantidad"]]
         chart_data = pd.DataFrame(
-            {"Departamentos": list(range(lista1)), "Cantidad": np.random.randn(25)}
+            {"Departamentos": (range(lista1)), "Cantidad": np.random.randn(25)}
         )
         st.bar_chart(
             chart_data, x="Departamentos", y=["Cantidad"]

5c8993a4814824746dc887d5809f574d90e28437
JimenaVegaS <89529401+JimenaVegaS@users.noreply.github.com>
2023-11-28 19:03:05 -0500

    g

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from PIL import Image
 import altair as alt
 
 #data del 2022
-df4= pd.read_csv("marzo2022de18a80años(1).csv")
+df4= pd.read_csv("marzo2022de18a80años.csv")
 df5= pd.read_csv("Junio2022de18a80años.csv")
 df6= pd.read_csv("septiembre2022de18a80años.csv")
 df7= pd.read_csv("diciembre2022de18a80años.csv")
